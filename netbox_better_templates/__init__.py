f2560934d7efee2856d340c872bf9e523df8f0c8
Mohammad Heydari <m_heydari@razi.cc>
2026-06-17 15:49:49 +0330

    updated_mainfest_to_match_org_rules

--- netbox_better_templates/__init__.py
+++ netbox_better_templates/__init__.py
@@ -4,16 +4,30 @@ from .monkey_patches import (
     patch_export_templates,
     patch_custom_links,
 )
-from ._version import get_version
+
+__version__ = "1.0.2"
+__description__ = "Adds some functionality to netbox templates and config render."
+__authors__ = [
+    {"name": "devixa-dev", "email": "hmohammad2520@gmail.com"},
+]
+__keywords__ = [
+    "python",
+    "netbox",
+    "provisioning",
+    "template",
+    "jinja",
+    "jinja2",
+]
+
 
 class NetboxBetterTemplatesConfig(PluginConfig):
-    name = 'netbox_better_templates'
-    verbose_name = 'Better Templates'
-    description = 'Adds new functionality to NetBox config templates.'
-    author = 'radin-system'
-    author_email = 'info@rsto.ir'
-    version = get_version()
-    base_url = 'better-templates'
+    name = "netbox_better_templates"
+    verbose_name = "Better Templates"
+    description = __description__
+    author = __authors__[0]["name"]
+    author_email = __authors__[0]["email"]
+    version = __version__
+    base_url = "better-templates"
 
     def ready(self):
         super().ready()
